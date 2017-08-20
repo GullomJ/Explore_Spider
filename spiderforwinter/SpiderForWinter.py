11797e1c479137ad44c83b58b95babdacb89f6fa
Atsukon Lee <urakiny@gmail.com>
2017-08-20 22:17:00 +0800

    添加注释:smile: :desert:

--- spiderforwinter/SpiderForWinter.py
+++ spiderforwinter/SpiderForWinter.py
@@ -18,7 +18,9 @@ class GetImage(object):
         """
 
         """
+        # 获取csv文件名称，列名
         self.excel_name, self.excel_column_name = self.get_input_from_client()
+        # 获取图片的url listS
         self.image_list = self.get_picurl_from_source()
 
     def get_pic(self):
@@ -44,7 +46,7 @@ class GetImage(object):
 
     def get_picurl_from_source(self):
         """
-
+        从csv文件中读取图片的url地址
         :return: 返回图片
         """
         try:
@@ -68,29 +70,50 @@ class GetImage(object):
             return local_path
 
     def get_input_from_client(self):
+        """
+        在控制台与用户交互，并获取csv文件的名称和需要使用的列名
+        :return:
+        """
         try:
             excel_name = raw_input('Please input csv name:')
             excel_column_name = raw_input('Please input Excel_column_name:')
-
+            # 断言 变量都不为空
             assert excel_name is not None, excel_column_name is not None
+            # 对csv文件的存在路径进行检查
             path = self.get_excel_path(excel_name=excel_name)
+            # 断言路径存在
             assert path
+            # 返回csv文件名称和列名称
             return excel_name, excel_column_name
         except AssertionError as e:
+            # try方法中的断言异常被捕获，这里是出口
             raise MyException(message='Please enter the correct information or check excel name. ').re_message
+        # 捕获所有异常，这么做不明知
         except Exception as e:
+            #
             raise MyException(message=e.message).re_message
 
 
 class MyException(BaseException):
+    __doc__ = '自定义异常类型，对程序的异常进行处理'
+
     def __init__(self, message=None):
+        """
+        类初始化函数，初始化类的message属性，对其进行赋值
+        :param message:需要打印的异常信息
+        """
         self.message = message if message else 'System Error'
 
     @property
     def re_message(self):
+        """
+        使用@property装饰器，指定re_message函数为类的特性
+        :return:
+        """
         return self.message
 
 
+# 程序入口，实例化GetImage对象，并调用对象的获取图片方法
 if __name__ == '__main__':
     GetImage().get_pic()
     # 656开始
